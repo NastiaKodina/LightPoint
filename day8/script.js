0f2adbdf5079d510309f6dfd48b75cb230adf174
Nastia <nastia_kodina@mail.ru>
2016-07-20 00:09:20 +0300

    JS

---
+++ day8/script.js
@@ -0,0 +1,40 @@
+var basketModule = (function() {
+    var basket = [{item:'table', price:200,count:7},
+                 {item:'lamp', price:10,count:6},
+                 {item:'pencil', price:2,count:15},
+                 {item:'sofa', price:500,count:1},
+                 {item:'pillow', price:50,count:8}]; 
+    return { 
+        addItem: function(values) {
+            basket.push(values);
+        },
+        getItemCount: function() {
+            return basket.length;
+        },
+        deleteItem: function(id) {
+            return basket.splice(id,1);
+        },
+        getTotalItemsCount: function() {
+           var itemCount = this.getItemCount(),totalCount=0;
+            while(itemCount--){
+                totalCount+= basket[itemCount].count; 
+            }
+            return totalCount;
+        },
+        sortByAlphabetAndItemName: function(){
+            basket.sort(function(obj1, obj2) {
+                            if (obj1.item < obj2.item) return -1;
+                            if (obj1.item > obj2.item) return 1;
+                             return 0;});
+            return 1;
+        }, 
+        sortByPrice: function(){
+            basket.sort(function(obj1, obj2) {
+                return obj1.price-obj2.price;});
+            return 1;
+        }
+    }
+}());
+
+
+
