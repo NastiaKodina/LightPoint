9854f035f090724abe22407ebf11cba703c686ff
Nastia <nastia_kodina@mail.ru>
2016-08-03 11:29:45 +0300

    Android Map

---
+++ day16/TryingMap/TryingMap/MainActivity.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using static Android.Gms.Maps.GoogleMap;
+
+namespace TryingMap
+{
+    [Activity(Label = "TryingMap", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity,IOnMapReadyCallback, ILocationListener,IInfoWindowAdapter
+    {
+        private GoogleMap mMap;
+
+        Location _currentLocation;
+        LocationManager _locationManager;
+        string _locationProvider;
+        MarkerOptions _marker;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            Button btnNormal = FindViewById<Button>(Resource.Id.btnNormal);
+            Button btnSatellite = FindViewById<Button>(Resource.Id.btnSatellite);
+            Button btnTerrain = FindViewById<Button>(Resource.Id.btnTerrain);
+            Button btnHybrid = FindViewById<Button>(Resource.Id.btnHybrid);
+
+            btnNormal.Click += delegate { mMap.MapType = GoogleMap.MapTypeNormal; };
+            btnSatellite.Click += delegate { mMap.MapType = GoogleMap.MapTypeSatellite; };
+            btnTerrain.Click += delegate { mMap.MapType = GoogleMap.MapTypeTerrain; };
+            btnHybrid.Click += delegate { mMap.MapType = GoogleMap.MapTypeHybrid; };
+
+            InitializeLocationManager();
+
+            SetUpMap();
+
+        }
+
+        
+
+        void InitializeLocationManager()
+        {
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                _locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                _locationProvider = string.Empty;
+            }
+            OnLocationChanged(_locationManager.GetLastKnownLocation(_locationProvider));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+        }
+
+        private void SetUpMap()
+        {
+            if (mMap==null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
+            if (_currentLocation != null)
+            {
+                LatLng position = new LatLng(_currentLocation.Latitude, _currentLocation.Longitude);
+                CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(position, 10);
+                mMap.MoveCamera(camera);
+
+                _marker = new MarkerOptions()
+               .SetPosition(position)
+               .Draggable(true);
+
+                mMap.AddMarker(_marker);
+                mMap.SetInfoWindowAdapter(this);
+                geo(_currentLocation.Latitude, _currentLocation.Longitude);
+            }
+            mMap.MarkerClick += mMap_MarkerClick;
+            mMap.MarkerDragEnd += mMap_DragEnd;
+            mMap.MapClick += mMap_Click;
+        }
+
+        private void mMap_Click(object sender, GoogleMap.MapClickEventArgs e)
+        {
+            LatLng position = e.Point;
+            LatLng latlng = new LatLng(position.Latitude, position.Longitude);
+            mMap.Clear();
+            MarkerOptions newMarker = new MarkerOptions()
+                    .SetPosition(latlng)
+                    .Draggable(true);
+            
+            mMap.AddMarker(newMarker);
+            geo(position.Latitude, position.Longitude);
+          
+           // Toast.MakeText(this, "Could not reverse geo-code the location", ToastLength.Short).Show();
+        }
+        private void mMap_MarkerClick(object sender, GoogleMap.MarkerClickEventArgs e)
+        {
+            LatLng position = e.Marker.Position;
+            mMap.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(position,10));
+            mMap.SetInfoWindowAdapter(this);
+
+
+        }
+
+        private void mMap_DragEnd(object sender, GoogleMap.MarkerDragEndEventArgs e)
+        {
+            LatLng position = e.Marker.Position;
+            Console.WriteLine(position.ToString());
+        }
+
+        public async void geo(double lat, double lon)
+        {
+            Geocoder geocdr = new Geocoder(this);
+            Task<IList<Address>> getAddressTask = geocdr.GetFromLocationAsync(lat, lon, 5);
+            IList<Address> addresses = await getAddressTask;
+
+            if (addresses.Any())
+            {
+                Address addr = addresses.First();
+                Toast.MakeText(this, addr.Thoroughfare+"\n"+addr.AdminArea + ", " + addr.CountryName, ToastLength.Short).Show();
+            } 
+            else
+            {
+                Toast.MakeText(this, "Could not reverse geo-code the location", ToastLength.Short).Show();
+            }
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            _currentLocation = location;
+           /* Geocoder geocdr = new Geocoder(this);
+            Task<IList<Address>> getAddressTask = geocdr.GetFromLocationAsync(location.Latitude, location.Longitude, 5);
+            IList<Address> addresses = await getAddressTask;
+
+            if (addresses.Any())
+            {
+                Address addr = addresses.First();
+                Toast.MakeText(this, "shjds", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Could not reverse geo-code the location", ToastLength.Short).Show();
+            }*/
+        }
+
+        public void OnProviderDisabled(string provider) { }
+
+        public void OnProviderEnabled(string provider) { }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras) { }
+
+        public View GetInfoContents(Marker marker)
+        {
+            return null;
+        } 
+
+        public View GetInfoWindow(Marker marker)
+        {
+            View view = LayoutInflater.Inflate(Resource.Layout.info_window,null,false);
+            return view;
+        }
+    }
+}
+
---
+++ day16/TryingMap/TryingMap/obj/Debug/android/src/md5b853e3165b449b3300d78089edcecaf0/MainActivity.java
@@ -0,0 +1,131 @@
+package md5b853e3165b449b3300d78089edcecaf0;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.maps.OnMapReadyCallback,
+		android.location.LocationListener,
+		com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_onLocationChanged:(Landroid/location/Location;)V:GetOnLocationChanged_Landroid_location_Location_Handler:Android.Locations.ILocationListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onProviderDisabled:(Ljava/lang/String;)V:GetOnProviderDisabled_Ljava_lang_String_Handler:Android.Locations.ILocationListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onProviderEnabled:(Ljava/lang/String;)V:GetOnProviderEnabled_Ljava_lang_String_Handler:Android.Locations.ILocationListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onStatusChanged:(Ljava/lang/String;ILandroid/os/Bundle;)V:GetOnStatusChanged_Ljava_lang_String_ILandroid_os_Bundle_Handler:Android.Locations.ILocationListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_getInfoContents:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoContents_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"n_getInfoWindow:(Lcom/google/android/gms/maps/model/Marker;)Landroid/view/View;:GetGetInfoWindow_Lcom_google_android_gms_maps_model_Marker_Handler:Android.Gms.Maps.GoogleMap/IInfoWindowAdapterInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"";
+		mono.android.Runtime.register ("TryingMap.MainActivity, TryingMap, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("TryingMap.MainActivity, TryingMap, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
+	{
+		n_onMapReady (p0);
+	}
+
+	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+
+	public void onLocationChanged (android.location.Location p0)
+	{
+		n_onLocationChanged (p0);
+	}
+
+	private native void n_onLocationChanged (android.location.Location p0);
+
+
+	public void onProviderDisabled (java.lang.String p0)
+	{
+		n_onProviderDisabled (p0);
+	}
+
+	private native void n_onProviderDisabled (java.lang.String p0);
+
+
+	public void onProviderEnabled (java.lang.String p0)
+	{
+		n_onProviderEnabled (p0);
+	}
+
+	private native void n_onProviderEnabled (java.lang.String p0);
+
+
+	public void onStatusChanged (java.lang.String p0, int p1, android.os.Bundle p2)
+	{
+		n_onStatusChanged (p0, p1, p2);
+	}
+
+	private native void n_onStatusChanged (java.lang.String p0, int p1, android.os.Bundle p2);
+
+
+	public android.view.View getInfoContents (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoContents (p0);
+	}
+
+	private native android.view.View n_getInfoContents (com.google.android.gms.maps.model.Marker p0);
+
+
+	public android.view.View getInfoWindow (com.google.android.gms.maps.model.Marker p0)
+	{
+		return n_getInfoWindow (p0);
+	}
+
+	private native android.view.View n_getInfoWindow (com.google.android.gms.maps.model.Marker p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
